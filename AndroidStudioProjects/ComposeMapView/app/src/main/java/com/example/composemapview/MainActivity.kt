967cc845f4f46d176095cf35ba63c9d07149eadf
Joat <michaelpaterson194@gmail.com>
2024-03-08 15:51:25 +0000

    Feature Update:
    modified the Map application using boxWithConstraints to set the layout of the Map View
    still need to move the control Text field and button to the bottom of the screen
    also separating the input field from the map with the text field occupying 80.dp & the map filling the rest of the screen.

--- AndroidStudioProjects/ComposeMapView/app/src/main/java/com/example/composemapview/MainActivity.kt
+++ AndroidStudioProjects/ComposeMapView/app/src/main/java/com/example/composemapview/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.composemapview
 
+import android.content.Context
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.DisplayMetrics
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
@@ -22,8 +28,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.example.composemapview.ui.theme.ComposeMapViewTheme
@@ -39,24 +47,28 @@ class MainActivity : ComponentActivity() {
             ComposeMapViewTheme {
                 // A surface container using the 'background' color from the theme
                 Surface {
-                    Column {
-                        var lat by remember { mutableStateOf("0.0") }
-                        var lng by remember { mutableStateOf("0.0") }
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        var lat by remember { mutableStateOf("") }
+                        var lng by remember { mutableStateOf("") }
                         var geoPoint by remember { mutableStateOf(GeoPoint(0.0, 0.0)) }
 
-                        Row(
-                            modifier = Modifier
-                                .padding(bottom = 20.dp)
-                                .fillMaxWidth()
-                                .wrapContentHeight()
-                        ) {
-                            TextField(value = lat, onValueChange = {lat=it}, label = {Text("Latitude")}, modifier = Modifier.weight(1.0f))
-                            TextField(value = lng, onValueChange = {lng=it}, label = {Text("Longitude")}, modifier = Modifier.weight(1.0f))
-                            Button(onClick = { geoPoint = GeoPoint(lat.toDouble(), lng.toDouble()) }) {
-                                Text("View")
+                        BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+                            val screenHeightInDp = getScreenHeightInDp(this@MainActivity)
+                            val mapHeight = screenHeightInDp - 80 // Subtracting the height of the controls row
+                            MapComposable(geoPoint, mapHeight.dp)
+
+                            Row(
+                                modifier = Modifier
+                                    .height(80.dp)
+                                    .fillMaxWidth()
+                            ) {
+                                TextField(value = lat, onValueChange = {lat=it}, label = {Text("Latitude")}, modifier = Modifier.weight(1.0f))
+                                TextField(value = lng, onValueChange = {lng=it}, label = {Text("Longitude")}, modifier = Modifier.weight(1.0f))
+                                Button(onClick = { geoPoint = GeoPoint(lat.toDouble(), lng.toDouble()) }) {
+                                    Text("View")
+                                }
                             }
                         }
-                        MapComposable(modifier, geoPoint)
                     }
                 }
             }
@@ -64,22 +76,29 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun MapComposable(modifier: Modifier, geoPoint : GeoPoint) {
-    Column(modifier = /*padding(top = 20.dp).fillMaxWidth()*/modifier) {
-        AndroidView(factory = { ctx ->
-            Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+fun getScreenHeightInDp(context: Context): Int {
+    val displayMetrics = DisplayMetrics()
+    val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    windowManager.defaultDisplay.getMetrics(displayMetrics)
 
-            MapView(ctx).apply {
-                setClickable(true)
-                setMultiTouchControls(true)
-                setTileSource(TileSourceFactory.MAPNIK)
-                controller.setZoom(14.0)
-            }
-        },
-            update = { view -> view.controller.setCenter(geoPoint)}
-        )
-    }
+    val density = context.resources.displayMetrics.density
+    return (displayMetrics.heightPixels / density).toInt()
+}
+
+@Composable
+fun MapComposable( geoPoint: GeoPoint, mapHeight: Dp) {
+    AndroidView(factory = { ctx ->
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
+
+        MapView(ctx).apply {
+            setClickable(true)
+            setMultiTouchControls(true)
+            setTileSource(TileSourceFactory.MAPNIK)
+            controller.setZoom(10.0)
+        }
+    },
+        update = { view -> view.controller.setCenter(geoPoint) }
+    )
 }
 
 @Preview(showBackground = true)

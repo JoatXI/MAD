d61b827dfc871aeebd8d99bd66d1046ffeaefa3a
Joat <michaelpaterson194@gmail.com>
2024-03-27 22:28:29 +0000

    SQLite COMPOSE UPDATE:
    modified the InputComposable button by creating two state variables use for displaying UI response
    modified the ADD SONG button to update the UI by using the result state variable to return the latest inserted song ID.

--- AndroidStudioProjects/SQLiteDemo/app/src/main/java/com/example/sqlitedemo/MainActivity.kt
+++ AndroidStudioProjects/SQLiteDemo/app/src/main/java/com/example/sqlitedemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -46,43 +47,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun InputComposable(songDao: BillboardDao) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        var id by remember { mutableStateOf("") }
+        var artistName by remember { mutableStateOf("") }
+        var songTitle by remember { mutableStateOf("") }
+        var songYear by remember { mutableStateOf("") }
 
-    var id by remember { mutableStateOf("") }
-    var songName by remember { mutableStateOf("") }
-    var songTitle by remember { mutableStateOf("") }
-    var songYear by remember { mutableStateOf("") }
+        var result by remember { mutableStateOf("") }
+        var song: Songs by remember { mutableStateOf(Songs(name = "", title = "", year = 0)) }
 
-    Column {
-        TextField(value = id, onValueChange = {id=it}, modifier = Modifier
-            .weight(1.0f)
-            .zIndex(2.0f)
-            .padding(end = 2.dp))
-        TextField(value = songName, onValueChange = {songName=it}, modifier = Modifier
-            .weight(1.0f)
-            .zIndex(2.0f)
-            .padding(end = 2.dp))
-        TextField(value = songTitle, onValueChange = {songTitle=it}, modifier = Modifier
-            .weight(1.0f)
-            .zIndex(2.0f)
-            .padding(end = 2.dp))
-        TextField(value = songYear, onValueChange = {songYear=it}, modifier = Modifier
-            .weight(1.0f)
-            .zIndex(2.0f)
-            .padding(end = 2.dp))
-    }
+        Column {
+            TextField(value = id, onValueChange = {id=it})
+            TextField(value = artistName, onValueChange = {artistName=it})
+            TextField(value = songTitle, onValueChange = {songTitle=it})
+            TextField(value = songYear, onValueChange = {songYear=it})
+        }
 
-    Row {
-        Button(onClick = {
-            val newSong = Songs(name = songName, title =  songTitle, year =  songYear.toInt())
-            val id = songDao.insert(newSong)
-        }) { Text("ADD SONG")}
-        Button(onClick = { songDao.getSongById(id = id.toLong()) }) { Text("FIND SONG")}
-        Button(onClick = {
-            val updSong = Songs(id = id.toLong(), name = songName, title = songTitle, year = songYear.toInt())
-            songDao.update(updSong)
-        }) { Text("UPDATE SONG")}
-        Button(onClick = {
-            val delSong = Songs(id = id.toLong(), name = songName, title = songTitle, year = songYear.toInt())
-            songDao.delete(delSong) }) { Text("DELETE SONG")}
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = {
+                val newSong = Songs(name = artistName, title =  songTitle, year =  songYear.toInt())
+                val id = songDao.insert(newSong)
+                result = id.toString()
+            }) { Text("ADD SONG")}
+            Text(result)
+
+            Button(onClick = { song = songDao.getSongById(id = id.toLong())!! }) { Text("FIND SONG")}
+            //Text(song.toString())
+        }
+
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = {
+                val updSong = Songs(id = id.toLong(), name = artistName, title = songTitle, year = songYear.toInt())
+                songDao.update(updSong)
+            }) { Text("UPDATE SONG")}
+            //Text("Song updated successfully!")
+
+            Button(onClick = {
+                val delSong = Songs(id = id.toLong(), name = artistName, title = songTitle, year = songYear.toInt())
+                songDao.delete(delSong) }) { Text("DELETE SONG")}
+            //Text("Song successfully deleted")
+        }
     }
 }
